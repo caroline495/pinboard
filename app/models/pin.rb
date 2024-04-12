b95f117b69fe2ba57fa464a7d85a5de77e817643
Caroline Zhang <cczhang1@yahoo.com>
2024-04-12 09:37:29 -0700

    update seed files to add board ids for all pins

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,8 +1,11 @@
 class Pin < ApplicationRecord
-
+    
+    # validates :creator_id, presence: true
+    # validates :board_id, allow_nil: true
     has_one_attached :image # all you need to allow pin to have photo attached
     # has_many_attached :photos // if you want to have many photos attached
     # we are not storing any information in pin table at all - activestorage handles it
+    
 
     belongs_to :creator,
         class_name: :User
--- db/seeds.rb
+++ db/seeds.rb
@@ -28,24 +28,25 @@ ApplicationRecord.transaction do
     user3 = User.create!(username: 'testUser7', email: 'testuser7@user.io', password: 'password')
   
     board1 = Board.create!(creator_id: user3.id, description: 'healthy and delicious', name: 'mediterranean inspired foods', private_mode: true)
-    board2 = Board.create!(creator_id: user1.id, description: 'ilnp spring collection', name: 'ILNP spring', private_mode: false)
+    board2 = Board.create!(creator_id: user1.id, description: 'ilnp spring collection', name: 'Testing', private_mode: false)
     board3 = Board.create!(creator_id: user3.id, description: 'sanrio lovers unite!', name: 'hello kitty', private_mode: false)
     board4 = Board.create!(creator_id: user3.id, description: 'brrr in Canada', name: 'Whistler', private_mode: false)
     board5 = Board.create!(creator_id: user3.id, description: "uncle iroh's tea shop", name: 'tea', private_mode: false)
     board6 = Board.create!(creator_id: user3.id, description: '', name: 'desserts', private_mode: true)
     board7 = Board.create!(creator_id: user3.id, description: '', name: 'coffee', private_mode: false)
+    board8 = Board.create!(creator_id: user2.id, description: '', name: 'home - california casual', private_mode: false)
 
-    pin1 = Pin.create!(creator_id: user1.id, description: 'testing', title: 'testing', link: "testing");
+    pin1 = Pin.create!(creator_id: user1.id, description: 'testing', title: 'testing', link: "testing", board_id: board2.id);
 
-    pin2 = Pin.create!(creator_id: user3.id, description: 'Nectarine summer salad', title: 'Nectarine summer salad', link: 'https://honestlyyum.com/11034/summer-nectarine-salad/');
+    pin2 = Pin.create!(creator_id: user3.id, description: 'Nectarine summer salad', title: 'Nectarine summer salad', link: 'https://honestlyyum.com/11034/summer-nectarine-salad/', board_id: board1.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/nectarine_summer_salad.jpg')
     pin2.image.attach(io: file, filename: 'nectarine_summer_salad.jpg');
 
-    pin3 = Pin.create!(creator_id: user2.id, description: 'flan recipe', title: 'Creamy caramel flan', link: 'https://www.tasteofhome.com/recipes/creamy-caramel-flan/');
+    pin3 = Pin.create!(creator_id: user2.id, description: 'flan recipe', title: 'Creamy caramel flan', link: 'https://www.tasteofhome.com/recipes/creamy-caramel-flan/', board_id: board6.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/caramel_flan.jpg')
     pin3.image.attach(io: file, filename: 'caramel_flan.jpg');
     
-    pin4 = Pin.create!(creator_id: user2.id, description: 'caramel lattes for the fall', title: 'Caramel latte recipe', link: 'https://bakingmischief.com/easy-caramel-latte/');
+    pin4 = Pin.create!(creator_id: user2.id, description: 'caramel lattes for the fall', title: 'Caramel latte recipe', link: 'https://bakingmischief.com/easy-caramel-latte/', board_id: board7.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/easy_caramel_latte.jpg')
     pin4.image.attach(io: file, filename: 'easy_caramel_latte.jpg');
 
@@ -61,23 +62,23 @@ ApplicationRecord.transaction do
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/greek_chicken_meatball_bowls.jpg')
     pin7.image.attach(io: file, filename: 'greek_chicken_meatball_bowls.jpg');
 
-    pin8 = Pin.create!(creator_id: user3.id, description: 'Love matcha and yuzu together...and combine it with gin!', title: 'Matcha Yuzu Gin Fizz', link: 'https://www.siftandsimmer.com/matcha-yuzu-gin-fizz/');
+    pin8 = Pin.create!(creator_id: user3.id, description: 'Love matcha and yuzu together...and combine it with gin!', title: 'Matcha Yuzu Gin Fizz', link: 'https://www.siftandsimmer.com/matcha-yuzu-gin-fizz/', board_id: board2.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/matcha_yuzu_gin_fizz.jpg')
     pin8.image.attach(io: file, filename: 'matcha_yuzu_gin_fizz.jpg');
 
-    pin9 = Pin.create!(creator_id: user3.id, description: '', title: 'Greek salad', link: 'https://downshiftology.com/recipes/greek-salad/');
+    pin9 = Pin.create!(creator_id: user3.id, description: '', title: 'Greek salad', link: 'https://downshiftology.com/recipes/greek-salad/', board_id: board1.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/best_greek_salad.jpg')
     pin9.image.attach(io: file, filename: 'best_greek_salad.jpg');
 
-    pin10 = Pin.create!(creator_id: user3.id, description: 'Vanilla lavender syrup to make lavender lattes!', title: 'Vanilla Lavender Syrup', link: 'https://www.lifewithtuyen.com/post/vanilla-lavender-syrup');
-    file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/vanilla_lavender_syrup.jpg', board_id: board7.id)
+    pin10 = Pin.create!(creator_id: user3.id, description: 'Vanilla lavender syrup to make lavender lattes!', title: 'Vanilla Lavender Syrup', link: 'https://www.lifewithtuyen.com/post/vanilla-lavender-syrup', board_id: board7.id);
+    file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/vanilla_lavender_syrup.jpg')
     pin10.image.attach(io: file, filename: 'vanilla_lavender_syrup.jpg');
 
-    pin11 = Pin.create!(creator_id: user3.id, description: 'celebrate spring with blackberry lavender scones', title: 'Blackberry Lavender White Chocolate Scones', link: 'https://www.halfbakedharvest.com/blackberry-lavender-white-chocolate-scones/');
+    pin11 = Pin.create!(creator_id: user3.id, description: 'celebrate spring with blackberry lavender scones', title: 'Blackberry Lavender White Chocolate Scones', link: 'https://www.halfbakedharvest.com/blackberry-lavender-white-chocolate-scones/', board_id: board6.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/blackberry_lavender_white_chocolate_scones.jpg')
     pin11.image.attach(io: file, filename: 'blackberry_lavender_white_chocolate_scones.jpg');
 
-    pin12 = Pin.create!(creator_id: user3.id, description: 'Lemon garlic baked chicken thighs - a delicious and simple main dish', title: 'Lemon Garlic Baked Chicken Thighs', link: 'https://www.eatwell101.com/garlic-lemon-baked-chicken-thighs-recipe');
+    pin12 = Pin.create!(creator_id: user3.id, description: 'Lemon garlic baked chicken thighs - a delicious and simple main dish', title: 'Lemon Garlic Baked Chicken Thighs', link: 'https://www.eatwell101.com/garlic-lemon-baked-chicken-thighs-recipe', board_id: board1.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/lemon_garlic_baked_chicken_thighs.jpg')
     pin12.image.attach(io: file, filename: 'lemon_garlic_baked_chicken_thighs.jpg');
 
@@ -89,31 +90,31 @@ ApplicationRecord.transaction do
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/matcha_jasmine_cake.jpg')
     pin14.image.attach(io: file, filename: 'matcha_jasmine_cake.jpg');
 
-    pin15 = Pin.create!(creator_id: user2.id, description: '', title: 'Salted caramel chocolate chip cookie bars', link: 'https://thatovenfeelin.com/salted-caramel-chocolate-chip-cookie-bars');
+    pin15 = Pin.create!(creator_id: user2.id, description: '', title: 'Salted caramel chocolate chip cookie bars', link: 'https://thatovenfeelin.com/salted-caramel-chocolate-chip-cookie-bars', board_id: board6.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/salted_caramel_chocolate_chip_cookie_bars.jpg')
     pin15.image.attach(io: file, filename: 'salted_caramel_chocolate_chip_cookie_bars.jpg');
 
-    pin16 = Pin.create!(creator_id: user2.id, description: 'California casual kitchen', title: 'california casual kitchen', link: 'https://thehavenlist.com/this-warm-minimalist-kitchen-is-california-casual-at-its-finest/');
+    pin16 = Pin.create!(creator_id: user2.id, description: 'California casual kitchen', title: 'california casual kitchen', link: 'https://thehavenlist.com/this-warm-minimalist-kitchen-is-california-casual-at-its-finest/', board_id: board8.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/california_casual_kitchen.jpg')
     pin16.image.attach(io: file, filename: 'california_casual_kitchen.jpg');
 
-    pin17 = Pin.create!(creator_id: user2.id, description: 'Camillestyles.com: Lauren Conrad’s Home Is the Definition of California Cool', title: 'california cool home', link: 'https://camillestyles.com/design/lauren-conrads-home-is-the-definition-of-california-cool/');
+    pin17 = Pin.create!(creator_id: user2.id, description: 'Camillestyles.com: Lauren Conrad’s Home Is the Definition of California Cool', title: 'california cool home', link: 'https://camillestyles.com/design/lauren-conrads-home-is-the-definition-of-california-cool/', board_id: board8.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/california_lauren_conrad_hallway.jpg')
     pin17.image.attach(io: file, filename: 'california_lauren_conrad_hallway.jpg');
 
-    pin18 = Pin.create!(creator_id: user2.id, description: 'Is This The New “California Casual”? Step Inside This Modern Double A Frame Home That Is Contemporary AND Dripping With Soul', title: 'Is This The New “California Casual”? Step Inside This Modern Double A Frame Home That Is Contemporary AND Dripping With Soul', link: 'https://stylebyemilyhenderson.com/blog/the-new-california-casual-modern-double-a-frame-home-tour');
+    pin18 = Pin.create!(creator_id: user2.id, description: 'Is This The New “California Casual”? Step Inside This Modern Double A Frame Home That Is Contemporary AND Dripping With Soul', title: 'Is This The New “California Casual”? Step Inside This Modern Double A Frame Home That Is Contemporary AND Dripping With Soul', link: 'https://stylebyemilyhenderson.com/blog/the-new-california-casual-modern-double-a-frame-home-tour', board_id: board8.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/california_upstairs_hallway.jpg')
     pin18.image.attach(io: file, filename: 'california_upstairs_hallway.jpg');
 
-    pin19 = Pin.create!(creator_id: user2.id, description: '41 Coastal Living Room Design Ideas You Havent Seen Before: Image 34 / 41', title: '41 Coastal Living Room Design Ideas', link: 'https://edwardgeorgelondon.com/en-usa/blogs/home-garden/coastal-living-room-design-ideas');
+    pin19 = Pin.create!(creator_id: user2.id, description: '41 Coastal Living Room Design Ideas You Havent Seen Before: Image 34 / 41', title: '41 Coastal Living Room Design Ideas', link: 'https://edwardgeorgelondon.com/en-usa/blogs/home-garden/coastal-living-room-design-ideas', board_id: board8.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/coastal_decor_living_room.jpg')
     pin19.image.attach(io: file, filename: 'coastal_decor_living_room.jpg');
 
-    pin20 = Pin.create!(creator_id: user2.id, description: 'modern coastal home with neutral palette', title: 'Modern coastal home', link: 'https://www.homestolove.com.au/home-tours/modern-coastal-home-with-neutral-palette-21808/');
+    pin20 = Pin.create!(creator_id: user2.id, description: 'modern coastal home with neutral palette', title: 'Modern coastal home', link: 'https://www.homestolove.com.au/home-tours/modern-coastal-home-with-neutral-palette-21808/', board_id: board8.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/coastal_home_banquette.jpg')
     pin20.image.attach(io: file, filename: 'coastal_decor_living_room.jpg');
 
-    pin21 = Pin.create!(creator_id: user2.id, description: 'From crate and barrel: tate dining table', title: 'modern dining room', link: 'https://www.crateandbarrel.com/tate-78-114-walnut-extendable-mid-century-dining-table/s683452');
+    pin21 = Pin.create!(creator_id: user2.id, description: 'From crate and barrel: tate dining table', title: 'modern dining room', link: 'https://www.crateandbarrel.com/tate-78-114-walnut-extendable-mid-century-dining-table/s683452', board_id: board8.id);
     file = URI.open('https://pinboard-project-seeds.s3.us-west-1.amazonaws.com/crate_and_barrel_tate_dining_table.jpg')
     pin21.image.attach(io: file, filename: 'coastal_decor_living_room.jpg');
     
